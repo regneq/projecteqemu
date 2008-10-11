7ab940972e7226bd81f31f49cbb9ab4d2359bc96
AndMetal <AndMetal@1db01234-8a6e-11dd-8937-b3a01dae0fbe>
2008-10-11 01:14:25 +0000

    Added all missing spell effects into spdat.h. No new effects supported yet.

--- trunk/EQEmuServer/zone/spdat.h
+++ trunk/EQEmuServer/zone/spdat.h
@@ -106,6 +106,8 @@ typedef enum {
 #define SE_SeeInvis					13
 #define SE_WaterBreathing			14
 #define SE_CurrentMana				15
+//#define SE_Unknown16				16	//not used
+//#define SE_Unknown17				17	//not used
 #define SE_Lull						18	// see SE_Harmony
 #define SE_AddFaction				19	// Alliance line
 #define SE_Blind					20
@@ -122,17 +124,24 @@ typedef enum {
 #define SE_Mez						31
 #define SE_SummonItem				32
 #define SE_SummonPet				33
+//#define SE_Unknown34				34	//not used
 #define SE_DiseaseCounter			35
 #define SE_PoisonCounter			36
+//#define SE_Unknown37				37	//not used
+//#define SE_Unknown38				38	//not used
+//#define SE_Unknown39				39	//not used
 #define SE_DivineAura				40
 #define SE_Destroy					41	// Disintegrate, Banishment of Shadows
 #define SE_ShadowStep				42
+//#define SE_Unknown43				43	//not used
 #define SE_Lycanthropy				44
+//#define SE_Unknown45				45	//not used
 #define SE_ResistFire				46
 #define SE_ResistCold				47
 #define SE_ResistPoison				48
 #define SE_ResistDisease			49
 #define SE_ResistMagic				50
+//#define SE_Unknown51				51	//not used
 #define SE_SenseDead				52
 #define SE_SenseSummoned			53
 #define SE_SenseAnimals				54
@@ -141,7 +150,9 @@ typedef enum {
 #define SE_Levitate					57
 #define SE_Illusion					58
 #define SE_DamageShield				59
+//#define SE_Unknown60				60	//not used
 #define SE_Identify					61
+//#define SE_Unknown62				62	//not used
 #define SE_WipeHateList				63
 #define SE_SpinTarget				64
 #define SE_InfraVision				65
@@ -149,7 +160,9 @@ typedef enum {
 #define SE_EyeOfZomm				67
 #define SE_ReclaimPet				68
 #define SE_TotalHP					69
+//#define SE_Unknown70				70	//not used
 #define SE_NecPet					71
+//#define SE_Unknown72				72	//not used
 #define SE_BindSight				73
 #define SE_FeignDeath				74
 #define SE_VoiceGraft				75
@@ -157,6 +170,7 @@ typedef enum {
 #define SE_LocateCorpse				77
 #define SE_AbsorbMagicAtt			78	// rune for spells
 #define SE_CurrentHPOnce			79	// Heals and nukes, non-repeating if in a buff
+//#define SE_Unknown80				80	//not used
 #define SE_Revive					81	//resurrect
 #define SE_SummonPC					82
 #define SE_Teleport					83
@@ -183,8 +197,10 @@ typedef enum {
 #define SE_Translocate				104
 #define SE_AntiGate					105	// Translocational Anchor
 #define SE_SummonBSTPet				106	// neotokyo: added BST pet support
+//#define SE_Unknown107				107	//not used
 #define SE_Familiar					108
 #define SE_SummonItemIntoBag		109	// Summon Jewelry Bag - summons stuff into container
+//#define SE_Unknown110				110	//not used
 #define SE_ResistAll				111
 #define SE_CastingLevel				112
 #define	SE_SummonHorse				113
@@ -196,15 +212,18 @@ typedef enum {
 #define SE_AttackSpeed3				119	// Frenzied Burnout
 #define SE_HealRate					120	// Packmaster's Curse - not sure what this is
 #define SE_ReverseDS				121
+//#define SE_Unknown122				122	//not used
 #define SE_Screech					123	// Form of Defense
 #define SE_ImprovedDamage			124
 #define SE_ImprovedHeal				125
+#define SE_IncreaseSpellResist		126 //not implemented
 #define SE_IncreaseSpellHaste		127
 #define SE_IncreaseSpellDuration	128
 #define SE_IncreaseRange			129
 #define SE_ReduceSpellHate			130
 #define SE_ReduceReagentCost		131
 #define SE_ReduceManaCost			132
+//#define SE_Unknown133				133	//not used
 #define SE_LimitMaxLevel			134
 #define SE_LimitResist				135
 #define SE_LimitTarget				136
@@ -215,7 +234,9 @@ typedef enum {
 #define SE_LimitInstant				141
 #define SE_LimitMinLevel			142
 #define SE_LimitCastTime			143
+//#define SE_Unknown144				144	//not used
 #define SE_Teleport2				145	// Banishment of the Pantheon
+//#define SE_Unknown146				146	//not used
 #define SE_PercentalHeal			147
 // solar: 
 // base is the effectid the command applies to
@@ -224,9 +245,11 @@ typedef enum {
 #define SE_StackingCommand_Block	148
 #define SE_StackingCommand_Overwrite 149
 #define SE_DeathSave				150
+#define SE_SuspendPet				151	//not implemented. Suspend Minion. base1: 0 = lose buffs & equip, 1 = keep buffs & equip
 #define SE_TemporaryPets			152	// Swarm of Fear III
 #define SE_BalanceHP				153	// Divine Arbitration
 #define SE_DispelDetrimental		154
+//#define SE_Unknown155				155	//not used
 #define SE_IllusionCopy				156	// Deception
 #define SE_SpellDamageShield		157	// Petrad's Protection
 #define SE_Reflect					158
@@ -241,6 +264,7 @@ typedef enum {
 #define SE_PetPowerIncrease			167	//not implemented, base % increase of pet stuff
 #define SE_MeleeMitigation			168	//not implemented, unlimited for duration
 #define SE_CriticalHitChance		169
+#define SE_SpellCritChance			170	//not implemented, +% to crit
 #define SE_CrippBlowChance			171
 #define SE_AvoidMeleeChance			172
 #define SE_RiposteChance			173
@@ -257,8 +281,10 @@ typedef enum {
 #define SE_HitChance				184
 #define SE_DamageModifier			185
 #define SE_MinDamageModifier		186
+//#define SE_Unknown187				187	//not used
 #define SE_IncreaseBlockChance		188	//not implemented
 #define SE_CurrentEndurance			189
+#define SE_EndurancePool			190	//not implemented
 #define SE_Amnesia					191	//Amnesia (Silence vs Melee Effect)
 #define SE_Hate2					192	//not implemented
 #define SE_SkillAttack				193
@@ -266,8 +292,8 @@ typedef enum {
 #define SE_StunResist				195
 #define SE_Strikethrough			196
 #define SE_SkillDamageTaken			197
-#define SE_Endurance2				198
-
+#define SE_CurrentEnduranceOnce		198
+//#define SE_Unknown199				199	//Flutter Flight (9480). something with aggro?
 #define SE_ProcChance				200
 #define SE_RangedProc				201	//not implemented
 #define SE_IllusionOther			202	//not implemented
@@ -276,46 +302,60 @@ typedef enum {
 #define SE_Rampage					205
 #define SE_AETaunt					206
 #define SE_FleshToBone				207
-
+//#define SE_Unknown208				208	//not used
 #define SE_FadingMemories2			209
 #define SE_PetShield				210	//per lucy, not implemented
 #define SE_AEMelee					211	//per lucy, not implemented
 #define SE_ProlongedDestruction		212	//per lucy, not implemented
-
+//#define SE_Unknown213				213	//not used
 #define SE_MaxHPChange 				214	//Grace of the Order, Plague of Hulcror, not implemented
-
+//#define SE_Unknown215				215	//not used
 #define SE_Accuracy					216	//not implemented
-
+//#define SE_Unknown217				217	//not used
 #define SE_PetCriticalHit			218 //aa effect
 #define SE_SlayUndead				219	//not implemented
-
+//#define SE_Unknown220				220	//not used
 #define SE_Packrat					221 //aa effect
-
+//#define SE_Unknown222				222	//not used
+//#define SE_Unknown223				223	//not used
 #define	SE_GiveDoubleRiposte		224 //aa effect
 #define SE_GiveDoubleAttack			225	//not implemented
 #define SE_TwoHandBash				226 //aa effect, bash with a 2h sword
 #define SE_ReduceSkillTimer			227	//not implemented
-
+//#define SE_Unknown228				228	//not used
 #define SE_PersistantCasting		229 //aa effect
-
+//#define SE_Unknown230				230	//not used
+//#define SE_Unknown231				231	//not used
 #define SE_DivineSave				232	//not implemented (base == % chance on death to insta-res)
-
+//#define SE_Unknown233				233	//not used
+//#define SE_Unknown234				234	//not used
 #define	SE_ChannelingChance			235 //Appears to only be used in AAs
-
+//#define SE_Unknown236				236	//not used
 #define SE_GivePetGroupTarget		237 //aaeffect, makes pets targetable by group spells
-
+//#define SE_Unknown238				238	//not used
+//#define SE_Unknown239				239	//not used
+//#define SE_Unknown240				240	//not used
+//#define SE_Unknown241				241	//not used
+//#define SE_Unknown242				242	//not used
+//#define SE_Unknown243				243	//not used
+//#define SE_Unknown244				244	//not used
+//#define SE_Unknown245				245	//not used
 #define SE_SetBreathLevel			246 //aa effect
-
+//#define SE_Unknown247				247	//not used
 #define SE_SecondaryForte			248 //aa effect, lets you gain a 2nd forte, gives you a 2nd specialize skill that can go past 50 to 100
 #define SE_SecondaryDmgInc			249 //aa effect, sinister strikes
-
+//#define SE_Unknown250				250	//not used
+//#define SE_Unknown251				251	//not used
+//#define SE_Unknown252				252	//not used
+//#define SE_Unknown253				253	//not used
 #define SE_Blank					254
-
+//#define SE_Unknown255				255	//not used
+//#define SE_Unknown256				256	//not used
 #define SE_PetDiscipline			257 //aa effect /pet hold
 #define SE_TripleBackstab			258 //not implemented
 #define SE_CombatStability			259 //aa effect
 #define SE_AddInstrumentMod			260
-
+//#define SE_Unknown261				261	//not used
 #define SE_RaiseStatCap				262 //not implemented
 #define	SE_TradeSkillMastery		263	//lets you raise more than one tradeskill above master.
 #define SE_HastenedAASkill			264 //aa skill
@@ -323,34 +363,43 @@ typedef enum {
 #define SE_ExtraAttackChance		266 //not implemented
 #define SE_PetDiscipline2			267 //aa effect /pet focus, /pet no cast
 #define SE_ReduceTradeskillFail		268 //aa effect, reduces chance to fail with given tradeskill by a percent chance
-
+//#define SE_Unknown269				269	//not used
+//#define SE_Unknown270				270	//not used
 #define SE_BaseMovementSpeed		271 //mods basemove speed, doesn't stack with other move mods, aa effect
 #define SE_CastingLevel2			272
 #define SE_CriticalDoTChance		273	//not implemented
 #define SE_CriticalHealChance		274	//not implemented
-
+//#define SE_Unknown275				275	//not used
 #define SE_Ambidexterity			276 //aa effect
-
+//#define SE_Unknown277				277	//not used
 #define	SE_FinishingBlow			278 //aa effect
 #define SE_Flurry					279	//not implemented
 #define SE_PetFlurry				280
-
+//#define SE_Unknown281				281	//not used
+//#define SE_Unknown282				282	//not used
+//#define SE_Unknown283				283	//not used
+//#define SE_Unknown284				284	//not used
+//#define SE_Unknown285				285	//not used
+#define SE_SpellDamage				286	//not implemented. adds direct spell damage
 #define SE_FocusCombatDurationMod	287
-
-#define SE_ImprovedSpellEffect		289 //This effect turns into this?
-
+//#define SE_Unknown288				288	//not used
+#define SE_ImprovedSpellEffect		289 //This effect turns into this? base1 = new spell id, happens when spell wears off
+//#define SE_Unknown290				290	//not used
 #define SE_Purify					291 //not implemented
-
+//#define SE_Unknown292				292	//not used
+//#define SE_Unknown293				293	//not used
 #define SE_CriticalSpellChance		294 //not implemented
-
+//#define SE_Unknown295				295	//not used
 #define SE_SpellVulnerability		296	//not implemented, base % increase in incoming spell damage
 #define SE_Empathy					297 //some kind of damage focus effect, maybe defensive?
 #define SE_ChangeHeight				298	//not implemented
 #define SE_WakeTheDead				299
 #define SE_Doppelganger				300
-
+//#define SE_Unknown301				301	//not used
+//#define SE_Unknown302				302	//not used
+//#define SE_Unknown303				303	//not used
 #define SE_OffhandRiposteFail		304 //aa effect, enemy cannot riposte offhand attacks
-#define SE_mitigateDamageShield		305 //not implemented
+#define SE_MitigateDamageShield		305 //not implemented
 #define SE_WakeTheDead2				306 //not implemented
 #define SE_Appraisal				307 //not implemented
 #define SE_SuspendMinion			308 //not implemented
@@ -358,17 +407,19 @@ typedef enum {
 #define SE_ReduceReuseTimer			310 //not implemented
 #define SE_NoCombatSkills			311 //not implemented
 #define SE_Sanctuary				312 //not implemented
-
+//#define SE_Unknown313				313	//not used
 #define SE_Invisibility2			314 //not implemented
 #define SE_InvisVsUndead2			315 //not implemented
+//#define SE_Unknown316				316	//not used
+//#define SE_Unknown317				317	//not used
 #define SE_ItemManaRegenCapIncrease	318 //aa effect increases amount of mana regen you can gain via items
 #define SE_CriticalHealOverTimer	319 //not implemented
-
+//#define SE_Unknown320				320	//not used
 #define SE_ReduceHate				321 //not implemented
 #define SE_GateToHomeCity			322 //not implemented
 #define SE_DefensiveProc			323	//not implemented
 #define SE_HPToMana					324 //not implemented
-
+//#define SE_Unknown325				325	//not used
 #define SE_SpellSlotIncrease		326 //aa effect, increases your spell slot availability
 #define SE_MysticalAttune			327 //AA effect, increases amount of buffs that a player can have
 #define SE_DelayDeath				328 //AA effect, increases how far you can fall below 0 hp before you die
@@ -378,10 +429,46 @@ typedef enum {
 #define SE_SummonToCorpse			332 //not implemented
 #define SE_EffectOnFade				333 //not implemented
 #define SE_BardAEDot				334	//needs a better name (spell id 703 and 730)
-
-#define SE_PercentXPIncrease		337 //not implemented
-#define SE_SummonAndResAllCorpses	338 //not implemented
-#define SE_TriggerOnCast			339 //not implemented
+#define SE_Unknown335				335	//not implemented. blocks next spell base1 times, then the spell fades after it reaches 0 (like Rune, but for spell types). Puratus (8494), Influence of Rage (8762) & Atta's Depuration (9815)
+//#define SE_Unknown336				336	//not used
+#define SE_PercentXPIncrease		337	//not implemented
+#define SE_SummonAndResAllCorpses	338	//not implemented
+#define SE_TriggerOnCast			339	//not implemented
+#define SE_LifeshardChance			340	//chance to create lifeshard
+//#define SE_Unknown341				341	//not used
+#define SE_Unknown342				342	//not sure. Call of Challenge (8271)
+#define SE_InterruptCasting			343	//not implemented. % chance to interrupt spells being cast every tic. Cacophony (8272)
+//#define SE_Unknown344				344	//not used
+//#define SE_Unknown345				345	//not used
+//#define SE_Unknown346				346	//not used
+//#define SE_Unknown347				347	//not used
+#define SE_Unknown348				348	//not implemented. looks like a rune based on how many times you cast a spell (cast a spell, decrease by 1. 0 = buff wears off)
+//#define SE_Unknown349				349	//not used
+#define SE_ManaBurn					350	//not implemented. no idea on the calculation
+										//from http://everquest.allakhazam.com/db/spell.html?spell=8452;page=1;howmany=50#m1184016444141306266 for Mana Blaze (base1 = 9000):
+										//"As a wizard with 1k AA's (all damage) and 13k mana, I always land this AA for 36k. It only seems to use about 10k mana."
+#define SE_PersistentEffect			351	//not implemented. creates a trap/totem that casts a spell (spell id + base1?) when anything comes near it
+#define SE_Unknown352				352	//not sure. looks to be some type of invulnerability? Test ITC (8755)
+#define SE_Unknown353				353	//not sure. looks to be some type of invulnerability? Test ISC (8756)
+#define SE_Unknown354				354	//not sure. looks to be some type of invulnerability? Test DAT (8757)
+#define SE_Unknown355				355	//not sure. looks to be some type of invulnerability? Test LT (8758)
+//#define SE_Unknown356				356	//not used
+//#define SE_Unknown357				357	//not used
+#define SE_CurrentManaOnce			358	//not implemented. increase/decrease mana once, like SE_CurrentHPOnce & SE_CurrentEnduranceOnce
+//#define SE_Unknown359				359	//not used
+#define SE_SpellOnKill				360	//not implemented. has a base1 % to cast spell base2 when you kill a "challenging foe" (>= max?)
+#define SE_Unknown361				361	//not sure. looks to be same as SpellOnKill, except for detrimental spells only? Test Proc 2 (9407)
+//#define SE_Unknown362				362	//not used
+//#define SE_Unknown363				363	//not used
+//#define SE_Unknown364				364	//not used
+#define SE_SpellOnDeath				365	//not implemented. casts base2 spell on the originator of the spell base1 % of the time when the person it was cast on dies. have to be >= max (level) for it to work?
+#define SE_Unknown366				366	//not sure. assume it has something to do with Corruption, maybe counters? wasn't implemented until Serpent's Spine, so not a big deal right now. Corr Test 1 (9428)
+#define SE_AddBodyType				367	//not implemented. adds body type of base1 so it can be affected by spells that are limited to that type (Plant, Animal, Undead, etc)
+#define SE_FactionMod				368	//not implemented. increases faction with base1 (faction id, live won't match up w/ ours) by base2
+#define SE_CorruptionCounter		369	//not implemented. wasn't added until Serpent's Spine, so we can't really do much w/ it
+#define SE_ResistCorruption			370	//not implemented. ditto
+//#define SE_Unknown371				371 //some type of slow? Aura of Inaction (9170)
+										//last effect
 
 #define DF_Permanent		50
 
