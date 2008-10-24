4f6fa7ea44433538ab4fe9eed072135f7fa69567
weissres <weissres@1db01234-8a6e-11dd-8937-b3a01dae0fbe>
2008-10-24 08:28:50 +0000

    Added SQL Files for some recent updates

---
+++ EQEmuServer/utils/sql/svn/142_deathpeace_and_lifetap_aas.sql
@@ -0,0 +1,11 @@
+INSERT INTO altadv_vars (skill_id, name, cost, max_level, hotkey_sid, hotkey_sid2, title_sid, desc_sid, type, spellid, prereq_skill, prereq_minpoints, spell_type, spell_refresh, classes, berserker, class_type, cost_inc) VALUES
+(844, 'Advanced Theft of Life', 5, 2, 2147483647, 2147483647, 2147483647, 2147483647, 6, 0, 201, 3, 0, 0, 2048, 0, 65, 0);
+
+INSERT INTO altadv_vars (skill_id, name, cost, max_level, hotkey_sid, hotkey_sid2, title_sid, desc_sid, type, spellid, prereq_skill, prereq_minpoints, spell_type, spell_refresh, classes, berserker, class_type, cost_inc) VALUES
+(1319, 'Soul Thief', 5, 3, 2147483647, 2147483647, 2147483647, 2147483647, 6, 0, 844, 2, 0, 0, 2048, 0, 65, 0);
+
+INSERT INTO altadv_vars (skill_id, name, cost, max_level, hotkey_sid, hotkey_sid2, title_sid, desc_sid, type, spellid, prereq_skill, prereq_minpoints, spell_type, spell_refresh, classes, berserker, class_type, cost_inc) VALUES
+(209, 'Death Peace', 5, 1, 2147483647, 2147483647, 2147483647, 2147483647, 6, 0, 0, 0, 0, 5, 2048, 0, 65, 0);
+
+INSERT INTO aa_actions (aaid, rank, reuse_time, spell_id, target, nonspell_action, nonspell_mana, nonspell_duration, redux_aa, redux_rate) VALUES
+(209, 0, 5, 5919, 0, 0, 0, 0, 0, 0); 
