fd8a30d9ea0f9ef90bda6c7b9bf579498c342cbd
congdar <congdar@1db01234-8a6e-11dd-8937-b3a01dae0fbe>
2008-11-09 02:12:13 +0000

    Add rule Character:BindAnywhere default false

--- trunk/EQEmuServer/common/ruletypes.h
+++ trunk/EQEmuServer/common/ruletypes.h
@@ -51,6 +51,7 @@ RULE_INT ( Character, ItemStunResistCap, 35)
 RULE_INT ( Character, ItemStrikethroughCap, 35)
 RULE_INT ( Character, SkillUpModifier, 100) //skill ups are at 100%
 RULE_BOOL ( Character, SharedBankPlat, false) //off by default to prevent duping for now
+RULE_BOOL ( Character, BindAnywhere, false)
 RULE_CATEGORY_END()
 
 RULE_CATEGORY( Guild )
