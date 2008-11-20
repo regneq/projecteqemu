f3850533afcbebed27ecca81e6da0cc10dba0e9e
mikeseveian <mikeseveian@1db01234-8a6e-11dd-8937-b3a01dae0fbe>
2008-11-20 08:23:13 +0000

    (Varkalas) WAR Sturdiness AA Fix

--- EQEmuServer/zone/client_mods.cpp
+++ EQEmuServer/zone/client_mods.cpp
@@ -253,9 +253,8 @@ sint32 Client::CalcMaxHP() {
 
 	nd += 150*GetAA(aaPlanarDurability);
 
-	nd += 100*GetAA(aaSturdiness);
-
 	max_hp = max_hp * nd / 10000;
+	max_hp += 100*GetAA(aaSturdiness);
 	max_hp += spellbonuses.HP;		
 	
 	if (cur_hp > max_hp)
