f79904d0b139f51246987d24747b656cbff53db7
weissres <weissres@1db01234-8a6e-11dd-8937-b3a01dae0fbe>
2008-11-22 22:31:42 +0000

    Added Account Limiting to allow limiting how many characters can be logged in per account at once.

--- trunk/EQEmuServer/common/ruletypes.h
+++ trunk/EQEmuServer/common/ruletypes.h
@@ -84,6 +84,8 @@ RULE_INT ( World, ExemptMaxClientsStatus, -1 ) //Lieka Edit: Exempt accounts fro
 RULE_INT ( World, AddMaxClientsPerIP, -1 ) //Trevius Edit: Maximum number of clients allowed to connect per IP address if account status is < ExemptMaxClientsStatus.  Default value: -1 (feature disabled)
 RULE_INT ( World, AddMaxClientsStatus, -1 ) //Trevius Edit: Accounts with status >= this rule will be allowed to use the amount of accounts defined in the AddMaxClientsPerIP.  Default value: -1 (feature disabled)
 RULE_BOOL ( World, ClearTempMerchantlist, true) //cavedude: Clears temp merchant items when world boots.
+RULE_INT ( World, AccountSessionLimit, -1 )  //Max number of characters allowed on at once from a single account (-1 is disabled)
+RULE_INT ( World, ExemptAccountLimitStatus, -1 )  //Min status required to be exempt from multi-session per account limiting (-1 is disabled)
 RULE_CATEGORY_END()
 
 RULE_CATEGORY( Zone )
