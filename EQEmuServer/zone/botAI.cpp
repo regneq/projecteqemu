0a204bd99a559c6707f2756a255c3ca8a936a47e
Angelox22 <Angelox22@1db01234-8a6e-11dd-8937-b3a01dae0fbe>
2009-02-19 14:07:25 +0000

    Fixed tracking: was crashing the zone and not working.

--- EQEmuServer/zone/botAI.cpp
+++ EQEmuServer/zone/botAI.cpp
@@ -1344,9 +1344,9 @@ void EntityList::ShowSpawnWindow(Client* client, int Distance, bool NamedOnly) {
 		{
 			if(iterator.GetData()->IsTrackable()) {
 				Mob* cur_entity = iterator.GetData();
-				if(cur_entity->IsBot() || cur_entity->IsPet() || cur_entity->IsFamiliar()) {
+				/* if(cur_entity->IsBot() || cur_entity->IsPet() || cur_entity->IsFamiliar()) {
 					continue;
-				}
+				} */  //Angelox: this breaks tracking and crashes the zone
 				const char *const MyArray[] = {
 					"a_","an_","Innkeep_","Barkeep_",
 					"Guard_","Merchant_","Lieutenant_",
