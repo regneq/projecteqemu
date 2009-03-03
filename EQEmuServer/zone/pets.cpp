19b3fa650f2b19aff58f9b7d7897fb67ba2be128
Angelox22 <Angelox22@1db01234-8a6e-11dd-8937-b3a01dae0fbe>
2009-03-03 14:56:33 +0000

    Fixed most (if not all) npc pet ghosting at zone safepoints.

--- EQEmuServer/zone/pets.cpp
+++ EQEmuServer/zone/pets.cpp
@@ -265,9 +265,11 @@ void Mob::MakePet(int16 spell_id, const char* pettype, const char *petname) {
 	entity_list.AddNPC(npc);
 	SetPetID(npc->GetID());
 }
-
+/* Angelox: This is why the pets ghost - pets were being spawned too far away from its npc owner and some
+into walls or objects (+10), this sometimes creates the "ghost" effect. I changed to +2 (as close as I 
+could get while it still looked good). I also noticed this can happen if an NPC is spawned on the same spot of another or in a related bad spot.*/
 Pet::Pet(NPCType *type_data, Mob *owner, PetType type, int16 spell_id)
-: NPC(type_data, 0, owner->GetX()+10, owner->GetY()+10, owner->GetZ(), owner->GetHeading())
+: NPC(type_data, 0, owner->GetX()+2, owner->GetY()+2, owner->GetZ(), owner->GetHeading())
 {
 	GiveNPCTypeData(type_data);
 	typeofpet = type;
