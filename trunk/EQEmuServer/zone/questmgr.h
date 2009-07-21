268daa0f1acddb390dbd8b561a5af47f92a8ad17
weissres <weissres@1db01234-8a6e-11dd-8937-b3a01dae0fbe>
2009-07-21 06:03:00 +0000

    Added type 4 option to quest::getlevel(type) to get max level ever reached. Added 3rd optional setting for quest::saylink() to allow link name to be different from the message said when clicking the link.

--- trunk/EQEmuServer/zone/questmgr.h
+++ trunk/EQEmuServer/zone/questmgr.h
@@ -207,7 +207,7 @@ public:
 	void FlagInstanceByRaidLeader(int32 zone, int16 version);
 	
 	const char* varlink(char* perltext, int item_id);
-	const char* saylink(char* Phrase, bool silent);
+	const char* saylink(char* Phrase, bool silent, char* LinkName);
 	void SetRunning(bool val);
 	bool IsRunning();
 	void FlyMode(int8 flymode);
