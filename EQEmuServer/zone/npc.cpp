3cf5adae3783298f94421e31f34d243aeb5af165
weissres <weissres@1db01234-8a6e-11dd-8937-b3a01dae0fbe>
2009-08-11 07:46:18 +0000

    (Secrets) Minor fix for the new Maxlevel Scaling for NPCs to correct a HP issue if they have visible armor/weapons or buffs.

--- EQEmuServer/zone/npc.cpp
+++ EQEmuServer/zone/npc.cpp
@@ -1594,7 +1594,8 @@ void NPC::LevelScale() {
 	if(level > 15 && level <= 25)
 		scale_adjust = 2;
 
-	max_hp += (max_hp * scaling);
+	base_hp += (int)(base_hp * scaling);
+	max_hp += (int)(max_hp * scaling);
 	cur_hp = max_hp;
 	STR += (int)(STR * scaling / scale_adjust);
 	STA += (int)(STA * scaling / scale_adjust);
