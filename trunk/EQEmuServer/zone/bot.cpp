8d2817b71c6699fa8d3d287ac36ede1815df4ddc
queluum <queluum@1db01234-8a6e-11dd-8937-b3a01dae0fbe>
2009-08-23 16:31:25 +0000

    Bug fix caused by bots ghosting when moving from  a small code block deleted in r933.

--- trunk/EQEmuServer/zone/bot.cpp
+++ trunk/EQEmuServer/zone/bot.cpp
@@ -1028,6 +1028,10 @@ bool Bot::Process() {
 	SpellProcess();
 
 	if (tic_timer.Check()) {
+		//60 seconds, or whatever the rule is set to has passed, send this position to everyone to avoid ghosting
+		if(/*qglobal_position_update_timer.Check() && */!moving)
+			SendAllPosition();
+
 		BuffProcess();
 
 		if(curfp)
