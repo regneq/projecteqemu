5968f624189c2b045bf8a8e5de1705e130037a83
KimmySprite <KimmySprite@1db01234-8a6e-11dd-8937-b3a01dae0fbe>
2009-10-15 04:25:49 +0000

    Makefile update + missed 2 files.

---
+++ trunk/EQEmuServer/zone/QGlobals.cpp
@@ -0,0 +1,134 @@
+#include "../common/debug.h"
+#include "../common/MiscFunctions.h"
+#include "QGlobals.h"
+#include "zonedb.h"
+
+void QGlobalCache::AddGlobal(uint32 id, QGlobal global)
+{
+	qGlobalBucket[id] = global;
+}
+
+/*void QGlobalCache::RemoveGlobal(std::string name)
+{
+	std::map<uint32, QGlobal>::iterator iter = qGlobalBucket.begin();
+	while(iter != qGlobalBucket.end())
+	{
+		if(name.compare(iter->second) == 0)
+		{
+			qGlobalBucket.erase(iter);
+			return;
+		}
+		++iter;
+	}
+}*/
+
+std::map<uint32, QGlobal> QGlobalCache::Combine(std::map<uint32, QGlobal> cacheA, std::map<uint32, QGlobal> cacheB, uint32 npcID, uint32 charID, uint32 zoneID)
+{
+	std::map<uint32, QGlobal> returnCache;
+	std::map<uint32, QGlobal>::iterator iter = cacheA.begin();
+	while(iter != cacheA.end())
+	{
+		QGlobal cur = iter->second;
+		bool rep = false;
+
+		if((cur.npc_id == npcID || cur.npc_id == 0) && (cur.char_id == charID || cur.char_id == 0) && (cur.zone_id == zoneID || cur.zone_id == 0))
+		{
+			if(Timer::GetCurrentTime() < cur.expdate)
+			{
+				returnCache[iter->first] = cur;
+			}
+		}	
+		++iter;
+	}
+	
+	iter = cacheB.begin();
+	while(iter != cacheB.end())
+	{
+		QGlobal cur = iter->second;
+		if((cur.npc_id == npcID || cur.npc_id == 0) && (cur.char_id == charID || cur.char_id == 0) && (cur.zone_id == zoneID || cur.zone_id == 0))
+		{
+			if(Timer::GetCurrentTime() < cur.expdate)
+			{
+				returnCache[iter->first] = cur;
+			}
+		}
+		++iter;
+	}
+	
+	return returnCache;
+}
+
+void QGlobalCache::LoadByNPCID(uint32 npcID)
+{
+	char errbuf[MYSQL_ERRMSG_SIZE];
+	char *query = 0;
+	MYSQL_RES *result;
+	MYSQL_ROW row;
+
+	if (database.RunQuery(query, MakeAnyLenString(&query, "select id, name, charid, npcid, zoneid, value, expdate"
+		" from quest_globals where npcid = %d", npcID), errbuf, &result))
+	{
+		while((row = mysql_fetch_row(result)))
+		{
+			AddGlobal(atoi(row[0]), QGlobal(std::string(row[1]), atoi(row[2]), atoi(row[3]), atoi(row[4]), row[5], row[6]?atoi(row[6]):0xFFFFFFFF));
+		}
+		mysql_free_result(result);
+	}
+	safe_delete_array(query);
+}
+
+void QGlobalCache::LoadByCharID(uint32 charID)
+{
+	char errbuf[MYSQL_ERRMSG_SIZE];
+	char *query = 0;
+	MYSQL_RES *result;
+	MYSQL_ROW row;
+
+	if (database.RunQuery(query, MakeAnyLenString(&query, "select id, name, charid, npcid, zoneid, value, expdate from"
+		" quest_globals where charid = %d && npcid = 0", charID), errbuf, &result))
+	{
+		while((row = mysql_fetch_row(result)))
+		{
+			AddGlobal(atoi(row[0]), QGlobal(std::string(row[1]), atoi(row[2]), atoi(row[3]), atoi(row[4]), row[5], row[6]?atoi(row[6]):0xFFFFFFFF));
+		}
+		mysql_free_result(result);
+	}
+	safe_delete_array(query);
+}
+
+void QGlobalCache::LoadByZoneID(uint32 zoneID)
+{
+	char errbuf[MYSQL_ERRMSG_SIZE];
+	char *query = 0;
+	MYSQL_RES *result;
+	MYSQL_ROW row;
+
+	if (database.RunQuery(query, MakeAnyLenString(&query, "select id, name, charid, npcid, zoneid, value, expdate from quest_globals"
+		" where zoneid = %d && npcid = 0 && charid = 0", zoneID), errbuf, &result))
+	{
+		while((row = mysql_fetch_row(result)))
+		{
+			AddGlobal(atoi(row[0]), QGlobal(std::string(row[1]), atoi(row[2]), atoi(row[3]), atoi(row[4]), row[5], row[6]?atoi(row[6]):0xFFFFFFFF));
+		}
+		mysql_free_result(result);
+	}
+	safe_delete_array(query);
+}
+void QGlobalCache::LoadByGlobalContext()
+{
+	char errbuf[MYSQL_ERRMSG_SIZE];
+	char *query = 0;
+	MYSQL_RES *result;
+	MYSQL_ROW row;
+
+	if (database.RunQuery(query, MakeAnyLenString(&query, "select id, name, charid, npcid, zoneid, value, expdate from quest_globals"
+		" where zoneid = 0 && npcid = 0 && charid = 0"), errbuf, &result))
+	{
+		while((row = mysql_fetch_row(result)))
+		{
+			AddGlobal(atoi(row[0]), QGlobal(std::string(row[1]), atoi(row[2]), atoi(row[3]), atoi(row[4]), row[5], row[6]?atoi(row[6]):0xFFFFFFFF));
+		}
+		mysql_free_result(result);
+	}
+	safe_delete_array(query);
+}
