3eb8e6d52d431859dc8956b736a79eb64029fae0
ringdahl.dan@gmail.com <ringdahl.dan@gmail.com@1db01234-8a6e-11dd-8937-b3a01dae0fbe>
2010-01-30 07:53:36 +0000

    added EQEmuLoginServer/EQCryptoAPI.h (contents from a EQEmu forum post by KLS)

---
+++ trunk/EQEmuServer/EQEmuLoginServer/EQCryptoAPI.h
@@ -0,0 +1,8 @@
+#ifndef EQEMUCAPI__H
+#define EQEMUCAPI__H
+
+char* DecryptUsernamePassword(const char* encryptedBuffer, unsigned int bufferSize, int mode);
+char* Encrypt(const char* buffer, unsigned int bufferSize, unsigned int &outSize);
+void _HeapDeleteCharBuffer(char *buffer);
+
+#endif
