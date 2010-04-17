b4c01a5b57fd9d8b5051417c5b869cd01fab7cd0
KimmySprite@gmail.com <KimmySprite@gmail.com@1db01234-8a6e-11dd-8937-b3a01dae0fbe>
2010-04-17 20:24:48 +0000

    AA with no spell associated with it should go on Cooldown now.

--- trunk/EQEmuServer/zone/AA.cpp
+++ trunk/EQEmuServer/zone/AA.cpp
@@ -263,6 +263,17 @@ void Client::ActivateAA(aaID activate){
 			SetMana(GetMana() - caa->mana_cost);
 		}
 		HandleAAAction(aaid);
+		if(caa->reuse_time > 0)
+		{
+			int32 timer_base = CalcAAReuseTimer(caa);
+			if(activate == aaImprovedHarmTouch || activate == aaLeechTouch)
+			{
+				p_timers.Start(pTimerHarmTouch, HarmTouchReuseTime);
+			}
+			p_timers.Start(AATimerID + pTimerAAStart, timer_base);
+			time_t timestamp = time(NULL);
+			SendAATimer(AATimerID, timestamp, timestamp);
+		}
 	}
 	
 	//cast the spell, if we have one
