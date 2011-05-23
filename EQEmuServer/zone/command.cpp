50da7d1c2efed9f785547d3df83b09e689d9107b
jeremiah.oligario <jeremiah.oligario@gmail.com>
2011-05-23 08:24:23 +0000

    I'm dumb...

--- EQEmuServer/zone/command.cpp
+++ EQEmuServer/zone/command.cpp
@@ -11078,13 +11078,12 @@ void command_camerashake(Client *c, const Seperator *sep)
 			worldserver.SendPacket(pack);
 			c->Message(13, "Successfully sent the packet to world! Shake it, world, shake it!");
 			safe_delete(pack);
-			return;
 		}
 		else {
 			c->Message(13, "Usage -- #camerashake [duration], [intensity [1-10])");
-			return;
 		}
 	}
+	return;
 }
 
 void command_disarmtrap(Client *c, const Seperator *sep) 
