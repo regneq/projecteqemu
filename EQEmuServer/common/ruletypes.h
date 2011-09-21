f6e90d069683e863dcce0c087b34ae21a3f9f229
weissres <weissres@gmail.com>
2011-09-21 04:11:17 +0000

    New rule added for specialization GM training on. #maxskills sets specializations to 50 instead of max. GM Trainers restrict specialization training correctly.

--- EQEmuServer/common/ruletypes.h
+++ EQEmuServer/common/ruletypes.h
@@ -109,6 +109,7 @@ RULE_CATEGORY( Skills )
 RULE_INT ( Skills, MaxTrainTradeskills, 21 )
 RULE_BOOL ( Skills, UseLimitTradeskillSearchSkillDiff, true )
 RULE_INT ( Skills, MaxTradeskillSearchSkillDiff, 50 )
+RULE_INT ( Skills, MaxTrainSpecializations, 50 )	// Max level a GM trainer will train casting specializations
 RULE_CATEGORY_END()
 
 RULE_CATEGORY( Pets )
