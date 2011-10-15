ba60d6799fd6734a2402813cd23984a50e44636b
jaredb <jaredb@daerma.com>
2011-10-15 04:25:10 +0000

    Added AE rampage message

--- EQEmuServer/zone/StringIDs.h
+++ EQEmuServer/zone/StringIDs.h
@@ -269,6 +269,7 @@
 #define STRIKETHROUGH_STRING		9078	//You strike through your opponent's defenses!
 #define SPELL_REFLECT				9082	//%1's spell has been reflected by %2.
 #define NEW_SPELLS_AVAIL			9149	//You have new spells available to you.  Check the merchants near your guild master.
+#define AE_RAMPAGE					11015	//%1 goes on a WILD RAMPAGE!
 #define FACE_ACCEPTED				12028	//Facial features accepted.
 #define SPELL_LEVEL_TO_LOW			12048	//You will have to achieve level %1 before you can scribe the %2.
 #define ATTACKFAILED				12158	//%1 try to %2 %3, but %4!
