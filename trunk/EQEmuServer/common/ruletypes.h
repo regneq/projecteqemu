d3e168f9be78051e776de7afdd04dcd42bd3dc7b
weissres <weissres@1db01234-8a6e-11dd-8937-b3a01dae0fbe>
2011-11-02 04:13:22 +0000

    Added Rule Character:EnableDiscoveredItems to make item discovery tracking optional.

--- trunk/EQEmuServer/common/ruletypes.h
+++ trunk/EQEmuServer/common/ruletypes.h
@@ -92,6 +92,7 @@ RULE_INT ( Character, SkillCapMaxLevel, 75 )	// Sets the Max Level used for Skil
 RULE_INT ( Character, StatCap, 0 )
 RULE_BOOL ( Character, CheckCursorEmptyWhenLooting, true ) // If true, a player cannot loot a corpse (player or NPC) with an item on their cursor
 RULE_BOOL ( Character, MaintainIntoxicationAcrossZones, true ) // If true, alcohol effects are maintained across zoning and logging out/in.
+RULE_BOOL ( Character, EnableDiscoveredItems, true ) // If enabled, it enables EVENT_DISCOVER_ITEM and also saves character names and timestamps for the first time an item is discovered.
 
 RULE_CATEGORY_END()
 
