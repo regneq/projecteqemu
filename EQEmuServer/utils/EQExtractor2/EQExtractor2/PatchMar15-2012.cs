908a04c16be62c95fc9065005d660c71651790a5
DerisionEQ <DerisionEQ@gmail.com>
2012-03-19 18:54:54 +0000

    Support for March 15 2012 client. Credit to fransick and Newby on the ShowEQ forums for the PP and most of the opcode changes.

---
+++ EQEmuServer/utils/EQExtractor2/EQExtractor2/PatchMar15-2012.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using EQExtractor2.InternalTypes;
+using EQExtractor2.OpCodes;
+using EQPacket;
+using MyUtils;
+
+namespace EQExtractor2.Patches
+{
+    class PatchMar152012Decoder : PatchNov172011Decoder
+    {
+        public PatchMar152012Decoder()
+        {
+            Version = "EQ Client Build Date March 15 2012.";
+
+            ExpectedPPLength = 29688;
+
+            PPZoneIDOffset = 22356;
+
+            PatchConfFileName = "patch_Mar15-2012.conf";
+        }               
+    }
+}
