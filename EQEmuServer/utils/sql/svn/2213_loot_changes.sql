ad657c5cb9629fcae054311399d54a6fd3bb982c
cavedude00 <cavedude00@1db01234-8a6e-11dd-8937-b3a01dae0fbe>
2012-12-15 06:31:21 +0000

    Changes to loot sql to prevent confusion.

--- EQEmuServer/utils/sql/svn/2213_loot_changes.sql
+++ EQEmuServer/utils/sql/svn/2213_loot_changes.sql
@@ -2,17 +2,5 @@ alter table loottable_entries add `droplimit` tinyint(2) unsigned NOT NULL defau
 alter table loottable_entries add `mindrop` tinyint(2) unsigned NOT NULL default 0;
 alter table lootdrop_entries change `chance` `chance` float not null default 1;
 alter table lootdrop_entries add `multiplier` tinyint(2) unsigned NOT NULL default 1;
-
-update lootdrop_entries lde 
-inner join loottable_entries lte ON lte.lootdrop_id = lde.lootdrop_id
-SET lde.chance = (lte.probability/100)*(lde.chance/100)*100;
-
-update loottable_entries set mindrop = multiplier where probability = 100;
-update loottable_entries set droplimit = multiplier where probability = 100;
-update loottable_entries set multiplier = 1 where probability = 100;
-
 update loottable_entries set droplimit = 1 where probability != 100;
 
-update lootdrop_entries set chance = 100 where chance > 100;
-
-alter table loottable_entries drop `probability`;
